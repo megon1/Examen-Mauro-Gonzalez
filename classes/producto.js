36e207c78b79dfa1210e3fc6612d02f3fb774111
Mauro Exequiel Gonzalez <86989774+megon1@users.noreply.github.com>
2025-10-17 22:50:41 -0300

    Update producto.js

--- classes/producto.js
+++ classes/producto.js
@@ -1,1 +1,25 @@
+class Producto {
+    static items = [];
 
+    constructor(codigo, nombre, precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // Método para agregar un producto a la lista estática
+    static agregar(producto) {
+        Producto.items.push(producto);
+    }
+
+    // eliminar un producto de la lista estática por código
+    static eliminar(codigo) {
+        const indice = Producto.items.findIndex(producto => producto.codigo === codigo);
+        if (indice !== -1) {
+            Producto.items.splice(indice, 1);
+            return true;
+        }
+        return false;
+    }
+
+}
